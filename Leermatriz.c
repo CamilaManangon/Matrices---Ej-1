1e644c0f85827e556cd97d4a2d7f55ab73c0175f
CamilaManangon <camila.manangon14@gmail.com>
2023-05-29 15:35:29 -0500

    Multiplicacion escalar por matriz y transpuesta

--- Leermatriz.c
+++ Leermatriz.c
@@ -7,6 +7,10 @@ void lecturaMatriz(int n, int m, int matriz[n][m]);
 
 void sumaMatriz(int n, int m, int matriz1[n][m],int matriz2[n][m],int matriz3[n][m]);
 
+void transpuesta(int n, int m, int matriz[n][m], int trans[m][n]);
+
+void multiEscalar(int n, int m, int matriz[n][m], int mult[n][m]);
+
 void lecturaMatriz(int n, int m, int matriz[n][m]){
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
@@ -24,20 +28,42 @@ void sumaMatriz(int n, int m, int matriz1[n][m],int matriz2[n][m],int matriz3[n]
     }
 }
 
+void transpuesta(int n, int m, int matriz[n][m], int trans[m][n]){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            trans[j][i] = matriz[i][j];
+        }
+    }
+}
+
+void multiEscalar(int n, int m, int matriz[n][m], int mult[n][m]){
+    int num;
+    printf("Ingrese el numero por el que quiera multiplicar la matriz: ");
+    scanf("%d",&num);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+             mult[i][j] = num * matriz[i][j];
+        }
+    }
+}
+
 
 int main(void){
     int matrizA[fil][col];
     int matrizB[fil][col];
     int suma[fil][col];
+    int trans[fil][col];
+    int multEsc[fil][col];
 
     printf("Matriz A: \n");
     lecturaMatriz(fil, col, matrizA);
-    printf("Matriz B: \n");
-    lecturaMatriz(fil, col, matrizB);
-    sumaMatriz(fil, col, matrizA, matrizB, suma);
+    //printf("Matriz B: \n");
+    //lecturaMatriz(fil, col, matrizB);
+    //sumaMatriz(fil, col, matrizA, matrizB, suma);
+    transpuesta(fil,col,matrizA,trans);
+    multiEscalar(fil, col, matrizA, multEsc);
 
-    printf("\nMatriz suma: \n");
-
+    /*printf("\nMatriz suma: \n");
     for(int i=0;i<fil;i++){
         for(int j=0;j<col;j++){
             printf("%d ",suma[i][j]);
@@ -45,7 +71,24 @@ int main(void){
                 printf("\n");
             }
         }
-    }    
+    }*/
+
+    printf("Matriz transpuesta\n");
+
+    for(int i=0;i<fil;i++){
+        for(int j=0;j<col;j++){
+            printf("%d ",trans[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("Multiplicacion escalar por matriz\n");
+    for(int i=0;i<fil;i++){
+        for(int j=0;j<col;j++){
+            printf("%d ",multEsc[i][j]);
+        }
+        printf("\n");
+    }
     
     /*printf("Matriz A:\n");
     for(int i=0;i<fil;i++){
